db0669dcab25b29d32038e080ab67f911125fdc0
Thyago Araújo Marques <thyagoaraujo186@gmail.com>
2025-11-20 03:44:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5050779012-77d86",
+  "appId": "1:657664191924:web:e20228805dcd2d9c5596e1",
+  "apiKey": "AIzaSyCJG5bJ0QFYT_eqMsTxkSTz0-7-GehcgiE",
+  "authDomain": "studio-5050779012-77d86.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "657664191924"
+};
