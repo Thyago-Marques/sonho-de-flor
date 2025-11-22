de625dcb3515a64635c7bc6aaecd8cc8a577ed2a
Thyago Araújo Marques <thyagoaraujo186@gmail.com>
2025-11-22 13:59:35 +0000

    remova o poupap de teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@
 import { PromotionalBanner } from "@/components/promotional-banner";
 import { CategoryButtons } from "@/components/category-buttons";
 import { ProductGrid } from "@/components/product-grid";
-import { TestPopup } from "@/components/test-popup";
 
 export default function Home() {
   return (
     <>
-      <TestPopup />
       <PromotionalBanner />
       <CategoryButtons />
       <ProductGrid />
